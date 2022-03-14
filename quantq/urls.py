9252fcd515e1e3036fa3906b5eea31d5d68b19b3
Rahul Krishna <57654450+rkrishnavedic@users.noreply.github.com>
2022-03-15 00:10:42 +0530

    init

---
+++ quantq/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import include
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('articles/', include('articles.urls')),
+    path('about/', views.about),
+    path('', views.homepage)
+]

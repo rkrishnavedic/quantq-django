e143b2611d280a81294c2de508913947988f6977
Rahul Krishna <57654450+rkrishnavedic@users.noreply.github.com>
2022-03-16 00:50:54 +0530

    update settings

--- quantq/settings.py
+++ quantq/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8iiwns@j@-b)##sjcxr^9fv)k76y+&xr+fl4_#bp)gp%k6zid8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
